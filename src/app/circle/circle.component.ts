68ed864ec0bb8d00440c15146c807cc22f3be440
Sathish Kalaimani <sathish.kalaimani@yahoo.in>
2018-03-20 09:28:27 +0530

    Popup Code

--- src/app/circle/circle.component.ts
+++ src/app/circle/circle.component.ts
@@ -2,51 +2,38 @@ import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { Circles } from './circles';
 import { CirclesService } from '../services/circles.service';
 
-@Component({
-  selector: 'app-circle',
-  templateUrl: './circle.component.html',
-  styleUrls: ['./circle.component.css']
-})
+@Component( {
+    selector: 'app-circle',
+    templateUrl: './circle.component.html',
+    styleUrls: ['./circle.component.css']
+} )
 
 
 export class CircleComponent implements OnInit {
 
- circles: Circles[];
- @Output() selectedCircle = new EventEmitter<any>();
+    circles: Circles[];
+    @Output() selectedCircle = new EventEmitter<any>();
 
- constructor(private circleService: CirclesService) { }
+    constructor( private circleService: CirclesService ) { }
 
-  getCircles() {
-    this.circleService.getCircles().subscribe(data => {this.circles = data.json(); });
-  }
-
-  selectCircle(circledata: string) {
-    const currentCircleName = { type : 'circle', value: 'circledata'};
-    this.selectedCircle.emit(currentCircleName);
-  }
-  ngOnInit() {
-    this.getCircles();   
-  }  
-  
-var modal = document.getElementById( 'myModal' );
-var btn = document.getElementById( "myButton" );
-var span = document.getElementsByClassName( "close" )[0];
-
-btn.onclick = function() {
-    modal.style.display = "block";
-}
-
-
-span.onclick = function() {
-    modal.style.display = "none";
-}
-
-window.onclick = function( event ) {
-    if ( event.target == modal ) {
-        modal.style.display = "none";
+    getCircles() {
+        this.circleService.getCircles().subscribe( data => { this.circles = data.json(); } );
     }
-}
 
+    selectCircle( circledata: string ) {
+        const currentCircleName = { type: 'circle', value: 'circledata' };
+        this.selectedCircle.emit( currentCircleName );
+    }
+    ngOnInit() {
+        this.getCircles();
+    }
+
+    openModal() {
+        document.getElementById( "myModal" ).style.display = "block";
+    }
+    closeModal() {
+        document.getElementById( "myModal" ).style.display = "none";
+    }
 }
 
 
